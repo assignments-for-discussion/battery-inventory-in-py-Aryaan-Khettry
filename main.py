3256a916797df500662c5a47eecd74c90cf51f83
Aryaan Khettry <97103701+Aryaan-Khettry@users.noreply.github.com>
2022-09-22 17:47:56 +0530

    Update main.py
    - Added code for the function "count_batteries_by_usage"
    - Added a few comments to improve understandability

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-
 def count_batteries_by_usage(cycles):
-  return {
+    # We use a dictionary, "usage_dict", to store the number of batteries of varying charge.
+  
+  usage_dict = {
     "lowCount": 0,
     "mediumCount": 0,
     "highCount": 0
   }
+  # Iterate through each battery's number of charge-cycles and increment the dictionary values according to the classification given.
 
+  for charge_cycles in cycles:
+        if charge_cycles < 0:
+            raise Exception("Value cannot be negative")
+        if charge_cycles >= 910:
+            usage_dict['highCount']+=1
+        elif charge_cycles >= 410:
+            usage_dict['mediumCount']+=1
+        elif charge_cycles >=0:
+            usage_dict['lowCount']+=1
+
+  return usage_dict
 
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
@@ -13,7 +26,7 @@ def test_bucketing_by_number_of_cycles():
   assert(counts["lowCount"] == 2)
   assert(counts["mediumCount"] == 3)
   assert(counts["highCount"] == 1)
-  print("Done counting :)")
+  print("Done counting, test case passed! :)")
 
 
 if __name__ == '__main__':
