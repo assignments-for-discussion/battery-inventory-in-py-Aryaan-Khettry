2c125765b7c53127f88fe3ab9d6eed1b672451e4
Aryaan Khettry <97103701+Aryaan-Khettry@users.noreply.github.com>
2022-09-22 17:57:43 +0530

    Update main.py
    Added a few more test cases

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+
 def count_batteries_by_usage(cycles):
     # We use a dictionary, "usage_dict", to store the number of batteries of varying charge.
-  
   usage_dict = {
     "lowCount": 0,
     "mediumCount": 0,
     "highCount": 0
   }
   # Iterate through each battery's number of charge-cycles and increment the dictionary values according to the classification given.
-
+  # Added tests to ensure no string values or negative values are passed to the function
   for charge_cycles in cycles:
+        if type(charge_cycles)!='int':
+            raise Exception("Please enter integer values only")
         if charge_cycles < 0:
             raise Exception("Value cannot be negative")
         if charge_cycles >= 910:
@@ -22,7 +24,7 @@ def count_batteries_by_usage(cycles):
 
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
-  counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
+  counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000,'f'])
   assert(counts["lowCount"] == 2)
   assert(counts["mediumCount"] == 3)
   assert(counts["highCount"] == 1)
@@ -31,3 +33,4 @@ def test_bucketing_by_number_of_cycles():
 
 if __name__ == '__main__':
   test_bucketing_by_number_of_cycles()
+
